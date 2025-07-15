c11058dc5fea483a6f4520988279ee2f962e37f2
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2025-07-16 01:27:08 +0530

    Implemented findbyUserid custom method to get the leaderboard based on user id

--- backend/src/main/java/com/goComet/gamingleaderboard/repository/LeaderboardRepository.java
+++ backend/src/main/java/com/goComet/gamingleaderboard/repository/LeaderboardRepository.java
@@ -4,6 +4,10 @@ import com.goComet.gamingleaderboard.entity.Leaderboard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LeaderboardRepository extends JpaRepository<Leaderboard, Long> {
+
+    Optional<Leaderboard> findByUserId(Long userId);
 }
