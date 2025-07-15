eb35065781c4d4d25338c60d23f58660358ea690
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2025-07-16 01:26:09 +0530

    Added custom query to getaverge score by uder id in game session repository

--- backend/src/main/java/com/goComet/gamingleaderboard/repository/GameSessionRepository.java
+++ backend/src/main/java/com/goComet/gamingleaderboard/repository/GameSessionRepository.java
@@ -2,6 +2,11 @@ package com.goComet.gamingleaderboard.repository;
 
 import com.goComet.gamingleaderboard.entity.GameSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface GameSessionRepository extends JpaRepository<GameSession, Long> {
+
+    @Query("SELECT AVG(gs.score) FROM GameSession gs WHERE gs.user.id = :userId")
+    Double getAverageScoreByUserId(@Param("userId") Long userId);
 }
