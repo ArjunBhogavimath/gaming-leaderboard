64dae881a9379b10068f393258ee9fda7bb67dc7
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2025-07-16 00:56:19 +0530

    leaderboard entity added

---
+++ backend/src/main/java/com/goComet/gamingleaderboard/entity/Leaderboard.java
@@ -0,0 +1,43 @@
+package com.goComet.gamingleaderboard.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "leaderboard")
+public class Leaderboard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", unique = true)
+    private User user;
+
+    @Column(name = "total_score")
+    private int totalScore;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(int totalScore) {
+        this.totalScore = totalScore;
+    }
+}
