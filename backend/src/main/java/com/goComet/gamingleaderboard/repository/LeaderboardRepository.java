31160c94ac47efe2df722490f7ad30ec2e9a2cc9
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2025-07-16 02:10:11 +0530

    wrote a custom native query for getting the rank by user id

--- backend/src/main/java/com/goComet/gamingleaderboard/repository/LeaderboardRepository.java
+++ backend/src/main/java/com/goComet/gamingleaderboard/repository/LeaderboardRepository.java
@@ -2,6 +2,8 @@ package com.goComet.gamingleaderboard.repository;
 
 import com.goComet.gamingleaderboard.entity.Leaderboard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,14 @@ import java.util.Optional;
 public interface LeaderboardRepository extends JpaRepository<Leaderboard, Long> {
 
     Optional<Leaderboard> findByUserId(Long userId);
+
+
+    @Query(value = """
+    SELECT r.rank FROM (
+      SELECT user_id, RANK() OVER (ORDER BY total_score DESC) AS rank
+      FROM leaderboard
+    ) r
+    WHERE r.user_id = :userId
+    """, nativeQuery = true)
+    Integer getRankByUserId(@Param("userId") Long userId);
 }
