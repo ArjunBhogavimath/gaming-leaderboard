7a60b68fbd1a79622a4d27808c97397508b32842
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2025-07-16 20:30:41 +0530

    fixed api url issue

--- backend/src/main/java/com/goComet/gamingleaderboard/controller/LeaderboardController.java
+++ backend/src/main/java/com/goComet/gamingleaderboard/controller/LeaderboardController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping(name = "leaderboard")
+@RequestMapping("api/leaderboard")
 public class LeaderboardController {
 
     @Autowired
